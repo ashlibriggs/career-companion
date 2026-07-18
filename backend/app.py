56d6589cc74c1f3b6a116293357a897d4197af64
Ashli Briggs <akent23@gmail.com>
2026-07-18 16:22:07 -0500

    Set up Flask backend foundation

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+
+@app.route("/")
+def home():
+    return {
+        "message": "Welcome to Career Companion API 🚀"
+    }
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
